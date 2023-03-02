f52eb23b7cd8c069495c6577184535ae94d60e8f
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-03-02 11:24:21 +0400

    Add functionality and some designs

--- src/components/Colorgenerator/ColorGenerator.jsx
+++ src/components/Colorgenerator/ColorGenerator.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 const ColorGenerator = () => {
-  return <div>ColorGenerator</div>;
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(false);
+  const [color, setColor] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(10);
+      setList(colors);
+      console.log(list);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-evenly">
+        <h1 className="text-3xl text-indigo-900 font-bold p-6">
+          Color Generator
+        </h1>
+        <form onSubmit={submit}>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 font-medium"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-2"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="grid grid-cols-4 gap-3 p-3">
+        {list.map((item, index) => {
+          return <SingleColor key={index} index={index} {...item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ColorGenerator;
--- src/components/Colorgenerator/SingleColor.jsx
+++ src/components/Colorgenerator/SingleColor.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import rgbToHex from "../../utils/rgbToHex";
 
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ rgb, index }) => {
+  const color = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  console.log(hex);
+  return (
+    <>
+      <div className="flex items-center flex-col border-2 p-5 gap-3 shadow-md rounded-md">
+        <div
+          className="w-24 h-24 rounded-full flex items-center justify-center cursor-pointer border-2 border-blue-400 hover:scale-110 duration-500"
+          style={{ background: `rgb(${color})`}}
+          onClick={() => {
+            navigator.clipboard.writeText(hex);
+          }}
+        >
+          <span className={index > 10 ? "text-white" : "text-black"}>copy</span>
+        </div>
+
+        <div>{hex}</div>
+      </div>
+    </>
+  );
 };
 
 export default SingleColor;
